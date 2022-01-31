cc47260fce57494df37dbeca2d31dc9a25b32331
ManelCast <94840343+ManelCast@users.noreply.github.com>
2022-01-31 14:00:48 +0100

    31_01_2022
    Añadido para moviles

---
+++ M5/index.php
@@ -0,0 +1,174 @@
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:b="http://www.google.com/2005/gml/b" xmlns:data="http://www.google.com/2005/gml/data" xmlns:expr="http://www.google.com/2005/gml/expr"><link type="text/css" id="dark-mode" rel="stylesheet" href=""><style type="text/css" id="dark-mode-custom-style"></style><head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<link href="Google%20Anime_files/1394523530-widget_css_bundle.css" rel="stylesheet" type="text/css">
+<meta content="width=350px" name="viewport">
+<link href="Google%20Anime_files/w3.css" rel="stylesheet">
+<link href="Google%20Anime_files/font-awesome.css" rel="stylesheet">
+<title>BUS!</title>
+<link rel="stylesheet" type="text/css" href="Google Anime/menu.css">
+<link href="layout.css" rel="stylesheet" type="text/css" media="all">
+<style id="page-skin-1" type="text/css"><!--
+/****CSS CODE*****/
+@import url(https://fonts.googleapis.com/css?family=Open+Sans);
+body{
+background: #c8c8c8 ; /*fafafa  fcfcfc  ffffff*/ 
+
+
+ background-size: 100% 200%;
+font-family: 'Open Sans', sans-serif;
+}
+#wrap{
+	padding-left: 30000px;
+}
+
+.search {
+width: 320;
+position: relative;
+margin: 0 auto;
+display: flex;
+}
+.searchTerm {
+width: 100%;
+border: 3px solid #2A80B9;
+border-right: none;
+padding: 5px;
+height:40px;
+border-radius: 5px 0 0 5px;
+outline: none;
+color: #9DBFAF;
+}
+.searchTerm:focus{
+color: white;
+}
+.searchButton {
+width: 40px;
+height: 40px;
+border: 1px solid #2A80B9;
+background: #2A80B9;
+text-align: center;
+color: #fff;
+border-radius: 0 5px 5px 0;
+cursor: pointer;
+font-size: 20px;
+}
+/*Resize the wrap to see the search bar change!*/
+.wdap{
+width: 20;
+position: absolute;
+top: 50%;
+left: 50%;
+transform: translate(-50%, -50%);
+}
+
+.tombo0{
+float: center;
+font-family:sans-serif;
+background: black;
+
+}
+
+.tombo0 img{
+	float:right;
+	margin: 0.3%;
+	margin-bottom: 20%;
+	margin-top:0%;
+	width: 30px;
+	height: 30px;
+}
+
+.tombol,input{
+background:#2C97DF;
+color:white;
+border-top:0;
+border-left:0;
+border-right:0;
+border-bottom:5px solid #2A80B9;
+padding:10px 10px;
+text-decoration:none;
+font-family:sans-serif;
+font-size:11pt;
+
+}
+
+.coso1{
+	float:right;
+}
+
+.tombo2{
+background: #ff9393;
+color:white;
+border-top:0;
+border-left:0;
+border-right:0;
+border-bottom:5px solid #e10e0e;
+padding:15px 20px;
+text-decoration:none;
+font-family:sans-serif;
+font-size:11pt;
+}
+
+li a img{
+	width: 30px;
+	height: 30px;
+	float:center;
+}
+.imana{
+width: 350;
+position: relative;
+margin: 1% auto;
+margin-bottom:0%;
+display: flex;
+
+}
+window.onload = choosePic;
+
+</style>
+<meta name="google-adsense-platform-account" content="ca-host-pub-1556223355139109">
+<meta name="google-adsense-platform-domain" content="blogspot.com">
+     <script src="script10.js"></script>
+     <link rel="stylesheet" href="script01.css">
+</head>
+<body width="400px" id="cuerpo">
+<header>
+</header>
+<div class="hello no-items section" id="hello">
+</div>
+<div class="wrap">
+
+<div class="wrapper row3">
+  <section class="hoc container clear"> 
+    <div class="group latest">
+
+      <article class="one_third first">
+        <figure><a href="m5_2.php"><img src="ganblog.png" alt=""></a></figure>
+        <div class="txtwrap">
+          <h4 class="heading">BUS Tarragona</h4>
+          Transport public de tarragona transportant en els alrededors de la ciutat e interior
+          <footer><hr><a href="m5_2.php">+info</a></footer>
+        </div>
+      </article>
+      <!--
+<article class="one_third">
+        <figure><a href="#"><img src="320x220.png" alt=""></a></figure>
+        <div class="txtwrap">
+          <h4 class="heading">Blog inicial</h4>
+          <p>Lorem</p>
+          <footer><hr><a href="#">+info</a></footer>
+        </div>
+      </article>
+      <article class="one_third">
+        <figure><a href="#"><img src="320x220.png" alt=""></a></figure>
+        <div class="txtwrap">
+          <h4 class="heading">Blog inicial</h4>
+          <p>Lorem</p>
+          <footer><hr><a href="#">+info</a></footer>
+        </div>
+      </article>-->
+       
+    </div>
+  </section>
+</div>
+<style>.pez img { width: 96px; position: absolute;transition: 1s;}body {overflow-x: hidden;} .pez{z-index: 99;}</style></body>
+<footer>
+	
+</footer>
